34a35e5e8d8a3736e2026703fc28b77c80a7b26d
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-04-15 17:15:49 -0300

    feat: Details view

---
+++ app/src/main/java/br/edu/ifgoiano/simulator/DetailActivity.kt
@@ -0,0 +1,19 @@
+package br.edu.ifgoiano.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifgoiano.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
