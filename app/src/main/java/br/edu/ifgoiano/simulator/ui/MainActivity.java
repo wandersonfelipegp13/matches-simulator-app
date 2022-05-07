13653e45edf5eeede1a3817c8fbdc92fb144554e
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-05-07 18:38:58 -0300

    swiperefreshing

--- app/src/main/java/br/edu/ifgoiano/simulator/ui/MainActivity.java
+++ app/src/main/java/br/edu/ifgoiano/simulator/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package br.edu.ifgoiano.simulator.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -54,6 +56,15 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        findMatchesFromApi();
+    }
+
+    private void setupMatchesRefresh() {
+        binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi);
+    }
+
+    private void findMatchesFromApi() {
+        binding.srlMatches.setRefreshing(true);
         matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
             @Override
             public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
@@ -64,21 +75,26 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     showErrorMessage();
                 }
+                binding.srlMatches.setRefreshing(false);
             }
 
             @Override
             public void onFailure(Call<List<Match>> call, Throwable t) {
                 showErrorMessage();
+                binding.srlMatches.setRefreshing(false);
             }
         });
     }
 
-    private void setupMatchesRefresh() {
-        // TODO Atualizar as partidas na ação de swipe.
-    }
-
     private void setupFloatingActionButton() {
-        // TODO Criar evento de click e simulação de partidas.
+        binding.fabSimulate.setOnClickListener(view -> {
+            view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    // TODO Implementar o algoritmo de simulação de partidas.
+                }
+            });
+        });
     }
 
     private void showErrorMessage() {
